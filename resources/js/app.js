f0eea896e8b92bb5f9b08c7a50d1bf0e3aaa01f4
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2020-11-08 09:10:09 +0900

    install ESLint

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import Vuetify from "../plugins/vuetify"
+import Vuetify from "../plugins/vuetify";
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -32,3 +32,7 @@ const app = new Vue({
     vuetify: Vuetify,
     el: '#app',
 });
+
+module.exports = function(a, b) {
+    return a + b;
+}
